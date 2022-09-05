ce1369f28cf6ea68a2f22e36d001d440af7fa7fb
Ibrahim Abdo <ibrahimporoo@gmail.com>
2022-09-05 06:07:48 +0200

    Updating to solute Some Problems

--- js/main.js
+++ js/main.js
@@ -13,6 +13,7 @@ linksItems.forEach(item => {
 	})
 })
 // Dealing With Menu Icon
+let theHeader = document.querySelector("header");
 let headerContent = document.querySelector("header .container .content");
 let menuIcon = document.querySelector("span.menu-icon");
 let closeIcon = document.querySelector("span.exit-icon");
@@ -20,14 +21,17 @@ let grayscale = document.querySelector("body .grayscale");
 
 
 menuIcon.addEventListener("click", _ => {
+	theHeader.style.overflow = "visible";
 	headerContent.style.right = "0";
 	grayscale.style.zIndex= "1";
 })
 closeIcon.addEventListener("click", _ => {
+	theHeader.style.overflow = "hidden";
 	headerContent.style.right = "-220px";
 	grayscale.style.zIndex= "-1";
 })
 grayscale.addEventListener("click", _ => {
+	theHeader.style.overflow = "hidden";
 	headerContent.style.right = "-220px";
 	grayscale.style.zIndex= "-1";
 	console.log("Clicked");
